0a8187e6f7a25c653919f0965e7010f139249a6d
Je-Carlos <cpjeanberg@gmail.com>
2023-08-24 17:57:32 -0300

    :pencil: limpar o buffer com nextLine()

--- 01-estrutura-sequencial/_03-entrada-de-dados/src/Main.java
+++ 01-estrutura-sequencial/_03-entrada-de-dados/src/Main.java
@@ -17,6 +17,21 @@ public class Main {
         System.out.printf("Valor double : %.2f%n" ,z);
         System.out.println("Valor char : " + a);
 
+        /* Quando você usa o nextLine() e da uma quebra de linha, essa quebra fica pendente na entrada padrão
+         * então você perderá nesse caso o s1, retornando vazio, se fizer um nextLine() vazio resolve, pois
+         * você limpa o buffer */
+        int d = sc.nextInt();
+        sc.nextLine();
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        String s3 = sc.nextLine();
+
+        System.out.println("Dados digitados: ");
+        System.out.println(d);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
         sc.close();
 
     }
