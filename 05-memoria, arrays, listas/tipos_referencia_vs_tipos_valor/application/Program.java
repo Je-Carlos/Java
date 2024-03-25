afb95a6008a29f46d5be2c7dfef048545e82b98f
Jean Carlos <cpjeanberg@gmail.com>
2024-03-24 21:06:37 -0300

    Comportamento de memória e Arrays

---
+++ 05-memoria, arrays, listas/tipos_referencia_vs_tipos_valor/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// tipo classe
+		int p;
+		p = 10;
+		/* necessário por algum valor dentro de p, pois a variavel precisa
+		 ter algum valor*/
+		System.out.println(p);
+
+		// tipo padrão
+		// Product p = new Product();
+		// ela é iniciada com um valor padrão (null, 0, 0.0, etc...)
+	}
+	
+
+}
